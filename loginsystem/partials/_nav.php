d2e92186d6fbe3ab1fcaac1a7f205b036eea807c
Arastu sharma <arastuom3@gmail.com>
2023-03-16 10:52:26 +0530

    changes in navbar

--- loginsystem/partials/_nav.php
+++ loginsystem/partials/_nav.php
@@ -6,7 +6,7 @@ else{
   $loggedin = false;
 }
 echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/INSTMS/loginsystem">Inter NIT Sports Management</a>
+  <a class="navbar-brand" >Inter NIT Sports Management</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -33,10 +33,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
        
       
     echo '</ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    
   </div>
 </nav>';
 ?>
